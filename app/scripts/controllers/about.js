8ecf77971efc355ec279ebd4570085735c1bfc7d
luisbahena <bahenalabz@gmail.com>
2017-04-07 22:48:37 -0700

    Generacion de proyecto.
    Esta es la base del proyecto.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectorhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectorhApp
+ */
+angular.module('proyectorhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectorhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyectorhApp
+ */
+angular.module('proyectorhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
